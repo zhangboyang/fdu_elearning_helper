c69cf06a8c77629bd7a6d8c21ade8975e34cd825
Zhang Boyang <zhangboyang.id@gmail.com>
2016-05-29 02:05:50 +0800

    change default prefs

--- xul/defaults/preferences/prefs.js
+++ xul/defaults/preferences/prefs.js
@@ -1,13 +1,13 @@
 pref("toolkit.defaultChromeURI", "chrome://elearninghelper/content/xulmain.xul");
 
 // enable remote debugging
-pref("devtools.chrome.enabled", true);
-pref("devtools.debugger.remote-enabled", true);
-pref("browser.dom.window.dump.enabled", true);
-pref("javascript.options.showInConsole", true);
-pref("javascript.options.strict", true);
-pref("nglayout.debug.disable_xul_cache", true);
-pref("nglayout.debug.disable_xul_fastload", true);
+pref("devtools.chrome.enabled", false);
+pref("devtools.debugger.remote-enabled", false);
+pref("browser.dom.window.dump.enabled", false);
+pref("javascript.options.showInConsole", false);
+pref("javascript.options.strict", false);
+pref("nglayout.debug.disable_xul_cache", false);
+pref("nglayout.debug.disable_xul_fastload", false);
 
 pref("general.useragent.override", "elearninghelper/unknownversion");
 
